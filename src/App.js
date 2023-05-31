e5e4edf8066ca343d4b8ba77924bda60155b50a9
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-06-01 00:13:35 +0530

    Implementing the cart Itemns page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ function App() {
     setCartIsShown(true);
   };
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCart}/>}
       <Header showCart={showCart} hideCart={hideCart}/>
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
