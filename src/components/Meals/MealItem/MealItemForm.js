a8c62f84957591790fa95a108ab300167c110461
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-05-31 19:03:23 +0530

    Modal added on sample Cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import Input from '../../UI/Input';
+import React from "react";
+import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = () => {
   return (
     <form className={classes.form}>
-        <Input label="Quantity" Input={{
-            id: "amount",
-            type: "number",
-            min: "1",
-            max: "9",
-            step: "1",
-            defaultValue: "1"
-        }}/>
-        <button> + Add</button>
+      <Input
+        label="Quantity"
+        Input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "9",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button> + Add</button>
     </form>
-  )
-}
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
